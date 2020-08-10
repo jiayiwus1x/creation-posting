71d35b762499bc4d204376bb3b986c23eb7a096f
test <jiayiwu.jmu@gmail.com>
2020-08-10 10:47:46 -0400

    fix navigational controller

---
+++ drawing/drawing/drawing/LoginViewControllers/LoginViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewController.swift
+//  firebase-app-tutorial
+//
+//  Created by Vishal Soni on 8/2/20.
+//  Copyright © 2020 Vishal Soni. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        
+        //Hide error label
+        errorLabel.alpha = 0
+        
+        // Style the elements
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(loginButton)
+        
+    }
+
+
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        // TODO: validate text fields
+        
+        // Create cleaned versions of text fields
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // signing in the user
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                //couldn't sign in
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else{
+                
+                let listViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.listViewController ) as! ListViewController
+                self.navigationController?.pushViewController(listViewController, animated: true)
+                
+//                self.view.window?.rootViewController = listViewController
+//                self.view.window?.makeKeyAndVisible()
+                
+            }
+        }
+        
+    }
+    
+    
+}
