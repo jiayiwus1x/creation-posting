38708e621481f828e3b541edecc885e5e76643a0
jiayi wu <jiayiwu.jmu@gmail.com>
2020-08-18 17:06:14 -0400

    remove firebase tutorial

--- login_signup/firebase-app-tutorial/Pods/Target Support Files/Pods-firebase-app-tutorial-firebase-app-tutorialUITests/Pods-firebase-app-tutorial-firebase-app-tutorialUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firebase_app_tutorial_firebase_app_tutorialUITests : NSObject
-@end
-@implementation PodsDummy_Pods_firebase_app_tutorial_firebase_app_tutorialUITests
-@end
--- login_signup/firebase-app-tutorial/Pods/Target Support Files/Pods-firebase-app-tutorial/Pods-firebase-app-tutorial-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firebase_app_tutorial : NSObject
-@end
-@implementation PodsDummy_Pods_firebase_app_tutorial
-@end
